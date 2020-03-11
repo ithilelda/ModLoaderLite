73bd56135d0c9678b22ad9a6c88d084f883a2fa0
Huawei Wang <huawei_007@yahoo.com>
2020-03-11 17:08:23 +0800

    added the ability to load custom xml.

--- ModLoaderLite/XmlEx/XmlEx.cs
+++ ModLoaderLite/XmlEx/XmlEx.cs
@@ -8,13 +8,15 @@ namespace ModLoaderLite.XmlEx
 {
     public static class XmlEx
     {
-        static Dictionary<Type, XmlSerializer> serializers = new Dictionary<Type, XmlSerializer>();
+        static Dictionary<string, XmlSerializer> serializers = new Dictionary<string, XmlSerializer>();
+
+        // this method is for loading vanilla valid xml. It has a <List> tag inside the root tag, so we have to wrap it around something.
         public static List<T> LoadXmlEx<T>(string localpath, string root, string item, string modpath = null, string pattern = "*.xml")
         {
-            var type = typeof(T);
             var defs_type = typeof(DefsEx<T>);
-            var ret = new List<T>();
-            if(!serializers.TryGetValue(type, out var serializer))
+            // we only create a new serializer if we can't find one for the type DefsEx<T>.
+            var id = Path.Combine(localpath, defs_type.ToString());
+            if (!serializers.ContainsKey(id))
             {
                 var type_attrs = new XmlAttributes();
                 var root_attr = new XmlRootAttribute(root);
@@ -27,35 +29,59 @@ namespace ModLoaderLite.XmlEx
                 var overrides = new XmlAttributeOverrides();
                 overrides.Add(defs_type, type_attrs);
                 overrides.Add(defs_type, "Defs", member_attrs);
-                serializer = new XmlSerializer(defs_type, overrides);
-                serializers.Add(type, serializer);
+                var serializer = new XmlSerializer(defs_type, overrides);
+                serializers.Add(id, serializer);
             }
-            if(string.IsNullOrEmpty(modpath))
+            return LoadXmlFile<DefsEx<T>>(localpath, modpath, pattern).SelectMany(i => i.Defs).ToList();
+        }
+        // this method is for loading fully custom xmls. Such xml doesn't contain the List tag, so we don't have to wrap it around anything, so we can directly desrialize into a list.
+        // providing option to specify the root element, so that you can wrap the list around anything.
+        // using XmlType attribute in T to specify your list element name, in case it is different than your class T name.
+        public static List<T> LoadXml<T>(string localpath, string root, string modpath = null, string pattern = "*.xml")
+        {
+            var type = typeof(List<T>);
+            var id = Path.Combine(localpath, type.ToString());
+            if (!serializers.ContainsKey(id))
             {
-                var paths = ModsMgr.Instance.GetPath(localpath).Where(pd => pd.mod != null).Select(pd => pd.path); // we ignore vanilla files.
-                foreach (var path in paths)
+                var serializer = new XmlSerializer(type, new XmlRootAttribute(root));
+                serializers.Add(id, serializer);
+            }
+            return LoadXmlFile<List<T>>(localpath, modpath, pattern).SelectMany(i => i).ToList();
+        }
+
+        static List<T> LoadXmlFile<T>(string localpath, string modpath, string pattern)
+        {
+            var ret = new List<T>();
+            var id = Path.Combine(localpath, typeof(T).ToString());
+            if (serializers.TryGetValue(id, out var serializer))
+            {
+                if (string.IsNullOrEmpty(modpath))
                 {
+                    var paths = ModsMgr.Instance.GetPath(localpath).Where(pd => pd.mod != null).Select(pd => pd.path); // we ignore vanilla files.
+                    foreach (var path in paths)
+                    {
+                        var files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
+                        foreach (var file in files)
+                        {
+                            using (var fs = new FileStream(file, FileMode.Open))
+                            {
+                                var defs = (T)serializer.Deserialize(fs);
+                                ret.Add(defs);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    var path = Path.Combine(modpath, localpath);
                     var files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
                     foreach (var file in files)
                     {
                         using (var fs = new FileStream(file, FileMode.Open))
                         {
-                            var defs = (DefsEx<T>)serializer.Deserialize(fs);
-                            ret.AddRange(defs.Defs);
+                            var defs = (T)serializer.Deserialize(fs);
+                            ret.Add(defs);
                         }
-                    }
-                }
-            }
-            else
-            {
-                var path = Path.Combine(modpath, localpath);
-                var files = Directory.GetFiles(path, pattern, SearchOption.AllDirectories);
-                foreach (var file in files)
-                {
-                    using (var fs = new FileStream(file, FileMode.Open))
-                    {
-                        var defs = (DefsEx<T>)serializer.Deserialize(fs);
-                        ret.AddRange(defs.Defs);
                     }
                 }
             }
